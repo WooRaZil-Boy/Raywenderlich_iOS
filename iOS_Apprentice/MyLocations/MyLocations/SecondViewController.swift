3288951d28ce7959e791a4c0fdf49d61a12d0edc
WooRaZil-Boy <byuncorry@hanmail.net>
2018-02-19 19:31:03 +0900

    finished 25_TagLocationScreen

--- iOS_Apprentice/MyLocations/MyLocations/SecondViewController.swift
+++ iOS_Apprentice/MyLocations/MyLocations/SecondViewController.swift
@@ -2,23 +2,23 @@
 //  SecondViewController.swift
 //  MyLocations
 //
-//  Created by 근성가이 on 2018. 2. 10..
-//  Copyright © 2018년 근성가이. All rights reserved.
+//  Created by Fahim Farook on 30/6/2017.
+//  Copyright © 2017 Razeware. All rights reserved.
 //
 
 import UIKit
 
 class SecondViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
 
 
 }

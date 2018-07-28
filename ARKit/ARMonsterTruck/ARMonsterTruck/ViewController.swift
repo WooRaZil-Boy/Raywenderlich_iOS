9bc0ae8accf944886666bb2666874da3bb1dc030
WooRaZil-Boy <byuncorry@hanmail.net>
2018-07-29 00:32:10 +0900

    finished Ch20_Advanced Game Physics

--- ARKit/ARMonsterTruck/ARMonsterTruck/ViewController.swift
+++ ARKit/ARMonsterTruck/ARMonsterTruck/ViewController.swift
@@ -31,6 +31,8 @@
 import UIKit
 import SceneKit
 import ARKit
+import CoreMotion //모션을 통해 트럭에 움직임을 준다. //모션 및 환경 관련 데이터 액세스
+//가속도, 자이로 스코프, 만보계, 자력계, 기압계 등의 정보를 포함한다.
 
 // MARK: - Game State
 enum GameState: Int16 {
@@ -49,6 +51,7 @@ class ViewController: UIViewController {
   var gameState: GameState = .detectSurface
   var focusPoint: CGPoint!
   var focusNode: SCNNode!
+  var groundNode: SCNNode! //무한 지면
   
   //트럭 노드들
   var truckNode: SCNNode!
@@ -68,8 +71,21 @@ class ViewController: UIViewController {
   let suspensionStiffness: CGFloat = 2.0 //바쿼와 차량 사이의 스프링 계수
   let suspensionCompression: CGFloat = 4.0
   //서스펜션이 압축 된 후 정지 길이로 돌아갈 때 속도의 제한 계수
-  
+    
   var physicsVehicle: SCNPhysicsVehicle! //Body와 Wheel을 합칠 차량 물리 객체
+  
+  var isThrottling = false //터치 입력을 사용해 플레이어가 트럭을 조정 중임을 나타내는 변수
+  var engineForce: CGFloat = 0 //조정 시에 엔진에 가해지는 힘의 양
+  let defaultEngineForce: CGFloat = 10.0 //조정 시 적용할 엔진 힘의 기본 값
+    
+  var brakingForce: CGFloat = 0 //브레이크
+  let defaultBrakingForce: CGFloat = 0.01 //트럭이 조정되지 않을 때 바퀴에 적용될 브레이크의 기본양
+    
+  let motionManager = CMMotionManager() //CoreMotion의 인스턴스. 모션 데이터를 얻는 데 사용한다.
+  let steeringClamp: CGFloat = 0.6 //차량을 제어하기 위해 앞 바퀴에 적용되는 각도
+  var steeringAngle: CGFloat = 0 //각도를 조정하여 값이 오버되는 것을 방지한다.
+    
+  var maximumSpeed: CGFloat = 2.0 //속도 제한 //단위는 시속 km
   
   // MARK: Outlets
   @IBOutlet var sceneView: ARSCNView!
@@ -88,6 +104,7 @@ class ViewController: UIViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    
     self.initSceneView()
     self.initScene()
     self.initARSession()
@@ -233,8 +250,7 @@ class ViewController: UIViewController {
   }
   
   func createVehiclePhysics() {
-    if physicsVehicle != nil {
-      //이전에 차량 물리 모델을 만들었다면 삭제 후 다시 만든다.
+    if physicsVehicle != nil { //이전에 차량 물리 모델을 만들었다면 삭제 후 다시 만든다.
       sceneView.scene.physicsWorld.removeBehavior(physicsVehicle)
     }
     let wheelFL = createPhysicsVehicleWheel(
@@ -252,12 +268,58 @@ class ViewController: UIViewController {
     //Helper 메서드를 사용해 바퀴 위치를 매개변수로 각각의 Wheel 객체를 만들어 준다.
     physicsVehicle = SCNPhysicsVehicle(
       chassisBody: truckNode.physicsBody!,
-      wheels: [wheelFL, wheelFR, wheelRL, wheelRR])
-    //차량 물리 모델로 객체 생성
+      wheels: [wheelFL, wheelFR, wheelRL, wheelRR]) //차량 물리 모델로 객체 생성
     sceneView.scene.physicsWorld.addBehavior(physicsVehicle)
     //해당 객체를 SceneView에 추가한다.
   }
-
+  
+  func createFloorNode() -> SCNNode {
+    //Creating an infinite ground plane
+    //감지된 평면에 할당된 물리 객체가 없으므로, 트럭을 생성해도 평면에 걸리지 않고 밑으로 떨어지게 된다.
+    //또 다른 문제는 감지된 면이 매우 작아 트럭이 움직일 공간이 적은 경우이다.
+    //이에 대한 해결책은 무한한 지면을 만드는 것이다. ScenenKit의 SCNFloor 노드를 활용한다.
+    let floorGeometry = SCNFloor()
+    floorGeometry.reflectivity = 0.25
+    //Floor 노드를 생성한 후 바닥을 완전히 반사로 만든다. 0.0이면 완전 비 반사
+    
+    let floorMaterial = SCNMaterial()
+    floorMaterial.diffuse.contents = UIColor.white //평면 백색 소재
+    floorMaterial.blendMode = .multiply //multiply로 설정하면, 바닥을 숨기고 반사와 그림자를 유지할 수 있다.
+    floorGeometry.materials = [floorMaterial]
+    
+    let floorNode = SCNNode(geometry: floorGeometry) //지오메트리 생성
+    floorNode.position = SCNVector3Zero
+    floorNode.physicsBody = SCNPhysicsBody(type: .static, shape: nil)
+    floorNode.physicsBody?.restitution = 0.5
+    floorNode.physicsBody?.friction = 4.0
+    floorNode.physicsBody?.rollingFriction = 0.0
+    //물리적 특성을 설정해 준다.
+    
+    return floorNode
+  }
+  
+  func startAccelerometer() {
+    // 1
+    guard motionManager.isAccelerometerAvailable else { return }
+    //디바이스가 해당 기능을 사용할 수 있는지 항상 확인하는 것이 좋다.
+    // 2
+    motionManager.accelerometerUpdateInterval = 1/60.0
+    //가속도계가 초당 60번 업데이트 된다. 따라서 동일한 속도로 업데이트
+    // 3
+    motionManager.startAccelerometerUpdates( //가속도계를 업데이트 한다.
+      to: OperationQueue.main,
+      withHandler: { (accelerometerData: CMAccelerometerData?,
+        error: Error?) in
+        //가속도계가 업데이트 될 때마다 콜백 핸들러가 실행된다.
+        self.updateSteeringAngle(acceleration:
+          accelerometerData!.acceleration)
+    })
+  }
+  
+  func stopAccelerometer() { //가속도계는 리소스를 많이 사용한다. 실제로 사용하지 않는다면, 멈춰주는 것이 좋다.
+    motionManager.stopAccelerometerUpdates()
+  }
+  
   // MARK: Update Functions
   
   func updateStatus() {
@@ -300,6 +362,7 @@ class ViewController: UIViewController {
     self.truckNode.position = self.focusNode.position
     self.truckNode.position.y += 0.20
     //트럭의 위치를 포커스 위치와 같게 하되, 지면보다 살짝 위(20cm)로 위치 시킨다.
+    
     self.truckNode.physicsBody?.velocity = SCNVector3Zero
     self.truckNode.physicsBody?.angularVelocity = SCNVector4Zero
     //트럭에 작용하는 물리 속성(속도, 각 속도)를 0으로 해 작용하는 힘이 없도록 초기화한다.
@@ -307,14 +370,100 @@ class ViewController: UIViewController {
     self.truckNode.physicsBody?.resetTransform()
     //트럭 노드의 위치를 업데이트 했기 때문에, 물리 시뮬레이션을 트럭의 새 위치로 업데이트해야 한다.
     //물리 객체에 resetTransform()를 호출하면 된다.
+    
+    // Update Ground Node
+    self.groundNode.position = self.focusNode.position
+    //트럭처럼 지면 노드를 보여주기 전에 초점 노드를 고려해야 한다. 지면 노드는 포커스 노드와 동일한 평면에 있어야 한다.
+    self.groundNode.physicsBody?.resetTransform() //물리 시뮬레이션 업데이트
   }
-
+  
+  override func touchesBegan(_ touches: Set<UITouch>,
+                             with event: UIEvent?) {
+    isThrottling = true
+  }
+  override func touchesEnded(_ touches: Set<UITouch>,
+                             with event: UIEvent?) {
+    isThrottling = false
+  }
+  //플레이어가 스크린에 닿았을 때 isThrottling는 true, 아니면 false
+  
+  func updateVehiclePhysics() {
+    guard self.gameState == .playGame else { return } //플레이 중에만 물리 속성 업데이트
+    
+    if isThrottling { //조정 중이라면
+      engineForce = defaultEngineForce //엔진 힘을 기본 값으로
+      brakingForce = CGFloat(0) //브레이크 값을 0으로
+    } else {
+      engineForce = CGFloat(0)
+      brakingForce = defaultBrakingForce //브레이크
+    }
+    
+    // Apply Engine Force
+    physicsVehicle.applyEngineForce(engineForce, forWheelAt: 0)
+    physicsVehicle.applyEngineForce(engineForce, forWheelAt: 1)
+    physicsVehicle.applyEngineForce(engineForce, forWheelAt: 2)
+    physicsVehicle.applyEngineForce(engineForce, forWheelAt: 3)
+    //applyEngineForce 메서드로 바퀴에 적용된다. 힘이 가해지는 바쿼 번호와 엔진 힘의 양을 적용
+    //적용되는 바퀴를 조절해 전륜, 후륜 구동 등으로 바꿔 줄 수 있다.
+    
+    // Apply Braking Force
+    physicsVehicle.applyBrakingForce(brakingForce, forWheelAt: 0)
+    physicsVehicle.applyBrakingForce(brakingForce, forWheelAt: 1)
+    physicsVehicle.applyBrakingForce(brakingForce, forWheelAt: 2)
+    physicsVehicle.applyBrakingForce(brakingForce, forWheelAt: 3)
+    //엔진 힘과 마찬가지로, 브레이크가 바퀴에 적용된다.
+    
+    // Apply Steering
+    physicsVehicle.setSteeringAngle(steeringAngle, forWheelAt: 0)
+    physicsVehicle.setSteeringAngle(steeringAngle, forWheelAt: 1)
+    //바퀴의 각도 조정 //앞 두 바퀴만 업데이트 //디바이스를 기울이면 각도에 따라 바퀴가 꺽이며 회전한다.
+    
+    // Limit Speed
+    if self.physicsVehicle.speedInKilometersPerHour >
+      CGFloat(maximumSpeed) {
+      //속도 제한을 초과하면 해당 업데이트에 대한 엔진힘이 취소된다.
+      //제한을 초과하면 엔진 힘이 다시 적용되고 트럭이 일정한 속도로 유지된다.
+      engineForce = CGFloat(0.0)
+    }
+  }
+  
+  func updateSteeringAngle(acceleration: CMAcceleration) {
+    steeringAngle = (CGFloat)(acceleration.y) //가속도 데이터의 y축 각도를 가져온다.
+    
+    if steeringAngle < -steeringClamp {
+      steeringAngle = -steeringClamp;
+    } else if steeringAngle > steeringClamp { //각도의 최대치
+      steeringAngle = steeringClamp;
+    }
+  }
+  
   // MARK: Game Management
   
   func startGame() {
+    guard self.gameState == .hitStartToPlay else { return }
+    //트럭이 Scene에 스폰되기 전에 hitStartToPlayState 상태여야 한다.
+    
+    DispatchQueue.main.async { //그래픽과 관련된 모든 작업은 메인 스레드에서 진행되어야 한다.
+      self.createVehiclePhysics() //물리 객체 생성
+      self.updatePositions() //포커스 노드와 동일한 위치로 업데이트
+      self.startAccelerometer() //가속도계 업데이트
+      self.groundNode.isHidden = false //지면 노드를 보이게 한다.
+      self.truckNode.isHidden = false //트럭 노드를 보이게 한다.
+      self.gameState = .playGame //게임 상태 전환
+    }
   }
   
   func resetGame(){
+    guard self.gameState == .playGame else { return }
+    //게임이 진행 중인 상태에서만 리셋을 할 수 있다.
+
+    DispatchQueue.main.async { //그래픽 업데이트는 항상 메인 스레드에서 진행해야 한다.
+      self.truckNode.isHidden = true
+      self.groundNode.isHidden = true
+      self.stopAccelerometer()
+      self.gameState = .detectSurface
+      //reset에 필요한 설정들을 한다.
+    }
   }
   
   func loadModels() {
@@ -343,6 +492,11 @@ class ViewController: UIViewController {
     
     truckNode.isHidden = true //트럭 노드를 숨겨 볼수 없도록 한다.
     sceneView.scene.rootNode.addChildNode(truckNode)
+    
+    // Load Ground Node
+    groundNode = self.createFloorNode() //지면 생성
+    groundNode.isHidden = true
+    sceneView.scene.rootNode.addChildNode(groundNode)
   }
 }
 
@@ -357,6 +511,7 @@ extension ViewController : ARSCNViewDelegate {
     DispatchQueue.main.async {
       self.updateStatus()
       self.updateFocusNode()
+      self.updateVehiclePhysics() //물리 엔진 업데이트
     }
   }
   
@@ -468,3 +623,9 @@ extension ViewController : ARSCNViewDelegate {
 
 
 
+//Challenge
+//MonsterTruck.scn에 광원을 추가해 부드러운 그림자를 생성해 줄 수 있다.
+//• Directional Light : 트럭 노드의 자식인 MonsterTruck.scn에 추가된 지향성 라이트는 그림자를 생성한다.
+//  트럭 노드의 자식 노드이므로 광원을 자동으로 로드한다. 광원은 트럭을 따라 가며 그림자를 만들 수 있다.
+//  보다 근본적이고 영구적인 해결책은 트럭 노드와 분리된 광원을 만들어야 하며, 트럭 주변을 따라 X, Z 위치를 업데이트한다.
+//• The floor node : SceneKit의 바닥 노드는 반사율을 조정할 수 있다. 0.25(25%) 정도면 사실적인 결과를 얻는다.

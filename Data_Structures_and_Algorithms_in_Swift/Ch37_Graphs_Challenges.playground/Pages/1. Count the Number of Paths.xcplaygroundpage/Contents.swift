eeffa7d79de84049fc33aa0d656a3726cc5ce611
WooRaZil-Boy <byuncorry@hanmail.net>
2020-01-31 00:08:49 +0900

    finished Ch37: Graphs Challenges

---
+++ Data_Structures_and_Algorithms_in_Swift/Ch37_Graphs_Challenges.playground/Pages/1. Count the Number of Paths.xcplaygroundpage/Contents.swift
@@ -0,0 +1,66 @@
+// Copyright (c) 2019 Razeware LLC
+// For full license & permission details, see LICENSE.markdown.
+/*:
+ # 1. Count the Number of Paths
+ 
+ Write a method to count the number of paths between two vertices in a directed graph.
+ */
+//방향 그래프에서 두 정점 사이의 경로 수를 세는 메서드를 작성한다.
+//ex. 아래 예에서는 A 에서 E 까지 5개의 경로가 있다.
+//: ![numberOfPaths](numberOfPaths.png)
+
+
+
+
+//Graph에서 두 정점 사이의 경로 수를 찾는 함수를 작성한다.
+//한 가지 해결 방법은 깊이 우선 순회(depth-first traversal)를 수행해 방문한 정점을 추적하는 것이다.
+extension Graph where Element: Hashable {
+  public func numberOfPaths(from source: Vertex<Element>, to destination: Vertex<Element>) -> Int {
+    var numberOfPaths = 0 //출발지와 목적지 사이에서 발견한 경로의 수를 추적한다.
+    var visited: Set<Vertex<Element>> = [] //방문한 모든 정점을 추적한다.
+    paths(from: source, to: destination, visited: &visited, pathCount: &numberOfPaths)
+    //helper 메서드를 재귀적으로 호출한다.
+    return numberOfPaths
+  }
+    
+    func paths(from source: Vertex<Element>, to destination: Vertex<Element>, visited: inout Set<Vertex<Element>>, pathCount: inout Int) {
+        visited.insert(source) //방문한 정점에 source(from)를 추가하면서 알고리즘을 시작한다.
+        if source == destination { //source와 destination가 같은지 확인한다.
+            pathCount += 1 //같다면, 경로가 존재하는 것이므로 pathCount를 1 추가한다.
+        } else { //source와 destination가 같지 않은 경우
+            let neighbors = edges(from: source) //source에서 모든 인접 간선을 가져온다.
+            for edge in neighbors {
+                //해당 source의 모든 간선에서 이전에 방문하지 않은 인접 정점을 재귀적으로 탐색하여 destination 정점에 대한 경로를 찾는다.
+                if !visited.contains(edge.destination) {
+                    paths(from: edge.destination, to: destination, visited: &visited, pathCount: &pathCount)
+                }
+            }
+        }
+        visited.remove(source)
+        //방문한 정점 set에서 source를 제거하여, 해당 node에 대한 다른 경로를 계속해서 찾을 수 있도록 한다.
+    }
+}
+//깊이 우선 그래프 탐색을 구현하고 있다. 목적지에 도달할 때까지 경로를 재귀적으로 진행하고, stack에서 pop하면서 추적한다.
+//시간 복잡도는 O(V+E)이다.
+
+let graph = AdjacencyList<String>()
+
+let a = graph.createVertex(data: "A")
+let b = graph.createVertex(data: "B")
+let c = graph.createVertex(data: "C")
+let d = graph.createVertex(data: "D")
+let e = graph.createVertex(data: "E")
+
+graph.add(.directed, from: a, to: b, weight: 0)
+graph.add(.directed, from: a, to: d, weight: 0)
+graph.add(.directed, from: a, to: e, weight: 0)
+graph.add(.directed, from: a, to: c, weight: 0)
+graph.add(.directed, from: b, to: d, weight: 0)
+graph.add(.directed, from: b, to: c, weight: 0)
+graph.add(.directed, from: d, to: e, weight: 0)
+graph.add(.directed, from: c, to: e, weight: 0)
+
+print(graph)
+print("Number of paths: \(graph.numberOfPaths(from: a, to: e))") // 5
+
+//: [Next Challenge](@next)
---
+++ Data_Structures_and_Algorithms_in_Swift/Ch37_Graphs_Challenges.playground/Pages/2. Graph your Friends.xcplaygroundpage/Contents.swift
@@ -0,0 +1,48 @@
+// Copyright (c) 2019 Razeware LLC
+// For full license & permission details, see LICENSE.markdown.
+//: [Previous Challenge](@previous)
+
+/*:
+ # 2. Graph your Friends
+ 
+ Vincent has three friends, Chesley, Ruiz, and Patrick. Ruiz has friends Ray, Sun, and a mutual friend with Vincent's. Patrick is friends with Cole, and Kerry. Cole is friends with Ruiz and Vincent. Create an adjacency list that represents this friendship graph. Which mutual friend do Ruiz and Vincent share?
+ */
+//Vincent는 Chesley, Ruiz, Patrick 세 명의 친구가 있다.
+//Ruiz는 Ray, Sun, Vincent의 친구이다.
+//Patrick은 Cole, Kerry의 친구이다.
+//Cole은 Ruiz, Vincent의 친구이다.
+//이 친구 관계를 그래프를 나타내는 adjacency list를 작성한다.
+//Ruiz와 Vincent 둘 모두의 친구는 누구인가?
+
+
+
+
+//AdjacencyList API를 사용해 구현한다. 0이 아닌 어떤 가중치(weight)를 사용해도 좋지만, 가장 좋은 기본값은 1이다.
+let graph = AdjacencyList<String>()
+
+let vincent = graph.createVertex(data: "vincent")
+let chesley = graph.createVertex(data: "chesley")
+let ruiz = graph.createVertex(data: "ruiz")
+let patrick = graph.createVertex(data: "patrick")
+let ray = graph.createVertex(data: "ray")
+let sun = graph.createVertex(data: "sun")
+let cole = graph.createVertex(data: "cole")
+let kerry = graph.createVertex(data: "kerry")
+
+graph.add(.undirected, from: vincent, to: chesley, weight: 1)
+graph.add(.undirected, from: vincent, to: ruiz, weight: 1)
+graph.add(.undirected, from: vincent, to: patrick, weight: 1)
+graph.add(.undirected, from: ruiz, to: ray, weight: 1)
+graph.add(.undirected, from: ruiz, to: sun, weight: 1)
+graph.add(.undirected, from: patrick, to: cole, weight: 1)
+graph.add(.undirected, from: patrick, to: kerry, weight: 1)
+graph.add(.undirected, from: cole, to: ruiz, weight: 1)
+graph.add(.undirected, from: cole, to: vincent, weight: 1)
+print(graph)
+
+//단순히 그래프를 확인해 서로 친구인지 확인할 수도 있다.
+print("Ruiz and Vincent both share a friend name Cole")
+//프로그램으로 이 문제를 해결하려면, 요소가 Hashable이라는 것을 사용해, Ruiz와 Vincent 간의 교집합(intersection)을 찾아 해결할 수 있다.
+let vincentsFriends = Set(graph.edges(from: vincent).map { $0.destination.data })
+let mutual = vincentsFriends.intersection(graph.edges(from: ruiz).map { $0.destination.data })
+print(mutual) // ["cole"]

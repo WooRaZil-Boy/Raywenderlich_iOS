7d16eec6b2e978eeb2830576d53206be35dba4da
WooRaZil-Boy <byuncorry@hanmail.net>
2020-03-05 22:38:28 +0900

    Edited Chapter 22: The Heap Data Structure

--- Data_Structures_and_Algorithms_in_Swift/Ch22_The_Heap_Data_Structure.playground/Contents.swift
+++ Data_Structures_and_Algorithms_in_Swift/Ch22_The_Heap_Data_Structure.playground/Contents.swift
@@ -1,5 +1,6 @@
 //Chapter22: The Heap Data Structure
 
+//인형 뽑기 기계에서 원하는 경품을 뽑는 것 처럼, 원하는 요소에 집중하는 것이 힙(Heap) 자료구조(Data Structure)의 본질이다.
 //힙을 사용하면, Collection에서 최소 및 최대 요소를 쉽게 가져올 수 있다.
 
 
@@ -11,29 +12,29 @@
 //cf. 포화 이진 트리(perfect binary tree) : 완전 이진 트리에서 마지막 level까지 모두 채워져 있는 트리
 //https://ko.wikipedia.org/wiki/%EC%9D%B4%EC%A7%84_%ED%8A%B8%EB%A6%AC
 //memory heap과는 다른 개념이다. memory heap은 pool of memory를 지칭하는 개념이다.
-//heap은 두 가지 성질이 있다.
-// 1. Max heap(최대 힙) : 상위 node가 항상 큰 vlaue를 가진다. root는 최대값.
-// 2. Min heap(최소 힙) : 상위 node가 항상 작은 vlaue를 가진다. root가 최소값.
+//heap은 두 가지 종류가 있다.
+// 1. Max heap(최대 힙) : 상위 node가 항상 큰 value를 가진다. root는 최대값.
+// 2. Min heap(최소 힙) : 상위 node가 항상 작은 value를 가진다. root가 최소값.
 
 
 
 
 //The heap property
-//heap에는 항상 충족되어야 하는 중요한 특성이 있다. 이를 heap invariant 또는 heap property 이라 한다. //p.227
-//최대 힙에서 상위 node는 항상 하위 child의 value보다 크거나 같은 값을 가져야 한다. root node는 항상 가장 큰 값을 가진다.
-//최소 힙에서 상위 node는 항상 하위 child의 value보다 작거나 같은 값을 가져야 한다. root node는 항상 가장 작은 값을 가진다.
-//힙의 또 다른 중요한 속성은 완전 이진 트리(complete binary tree)라는 것이다.
-//즉, 마지막 level을 제외한 모든 level이 채워져 있어야 한다. 현재 게임을 완료하기 전에 다음 단계로 넘어갈 수 없는 게임과 비슷하다.
+//힙(heap)은 항상 만족해야 하는 중요한 특성이 있다. 이를 heap invariant 또는 heap property 이라 한다. //p.227
+//최대 힙에서 상위 node는 항상 child의 value보다 크거나 같은 값을 가져야 한다. root node는 항상 가장 큰 값을 가진다.
+//최소 힙에서 상위 node는 항상 child의 value보다 작거나 같은 값을 가져야 한다. root node는 항상 가장 작은 값을 가진다.
+//힙의 또 다른 중요한 특징은 완전 이진 트리(complete binary tree)라는 것이다.
+//즉, 마지막 level을 제외한 모든 level이 채워져 있어야 한다. 현재 단계를 완료하기 전에 다음 단계로 넘어갈 수 없는 게임과 비슷하다.
 
 
 
 
 //Heap applications
 //힙을 사용하는 예시는 다음과 같다.
-// • Collection의 최소 또는 최대 요소 계산
-// • 힙 정렬
+// • Collection의 최소값 또는 최대값 요소 계산
+// • 힙 정렬(Heap sort)
 // • 우선 순위 큐(priority queue) 구성
-// • 우선 순위 큐(priority queue)를 사용하여 그래프 Prim 또는 Dijkstra와 같은 자료구조 구성
+// • Prim 또는 Dijkstra와 같은 그래프 자료구조 알고리즘을 우선 순위 큐(priority queue)를 사용해 구현
 
 
 
@@ -56,17 +57,18 @@ struct Heap<Element: Equatable> {
 //How do you represent a heap?
 //트리에는 child에 대한 참조를 저장하는 node가 있다. 이진 트리는 왼쪽과 오른쪽 child에 대한 참조를 가지고 있다.
 //Heap은 실제로는 이진트리이지만, 간단히 Array로 표현할 수 있다. 이는 tree를 만드는 특이한 방법인 것처럼 보인다.
-//그러나, 이런 힙 구현의 이점 중 하나는 힙의 요소가 모두 메모리에 함께 저장되므로(Array) 효율적인 시간, 공간 복잡도를 가진다는 것이다.
-//swap 요소가 힙에서 큰 역할을 하는데, 이 역시 이진 트리보다 Array로 구현하는 것이 더 쉽다.
+//그러나, 배열을 사용하는 이런 힙 구현의 이점 중 하나는 힙의 요소가 모두 메모리에 연이어 저장되므로(Array) 효율적인 시간, 공간 복잡도를 가진다는 것이다.
+//요소를 교환(swap)하는 것이 힙의 중요한 부분인데, 이 역시 이진 트리보다 Array로 구현하는 것이 더 쉽다.
 //heap을 Array로 나타내려면, 각 level의 요소를 왼쪽부터 오른쪽으로 반복하면 된다. //p.229
 //level이 증가하면, 이전 level보다 필요한 node가 두 배 많아 진다.
-//Array로 구현하면, heap의 모든 node에 쉽게 액세스할 수 있다. tree에서의 순회 대신, 간단한 수식을 사용해 Array의 node에 액세스할 수 있다.
+//Array로 구현하면, heap의 모든 node에 쉽게 액세스할 수 있다. tree에서의 순회 대신, 간단한 공식을 사용해 Array의 node에 액세스할 수 있다.
 //0부터 시작하는 index i가 있다고 하면
 // • 해당 node의 left child는 2i + 1 이다.
 // • 해당 node의 right child는 2i +2 이다.
 // • 해당 node의 parent node는 floor((i-1)/2) 이다.
 //node의 왼쪽, 오른쪽 child를 확인하기 위한 이진트리의 순회는 O(log n) 연산이다.
 //Array와 같은 random-access(임의 접근) data structure에서 동일한 작업은 O(1) 이다.
+//이를 고려하여, 위에서 구현한 기본 힙(heap)에 몇 가지 속성과 메서드를 추가한다.
 extension Heap {
     var isEmpty: Bool {
         elements.isEmpty
@@ -97,13 +99,13 @@ extension Heap {
 
 
 //Removing from a heap
-//basic remove 연산은 단순히 heap에서 root node를 제거한다.
-//최대 힙에서는 최대값을 제거한다. 이 작업을 위해서 먼저, root node와 힙의 마지막 요소를 교체해야 한다. //p.232
+//기본적인 제거(basic remove) 연산(operation)은 단순히 힙(heap)에서 root 노드(node)를 제거하면 된다.
+//최대 힙에서는 최대값을 제거한다. 이 작업을 위해서 먼저, root node와 힙의 마지막 요소를 교환해야 한다. //p.232
 //swap 이후, 마지막 요소(이전의 root, 최대값)을 제거하고, 필요한 경우 이 value를 따로 저장하여 반환할 수 있다.
 //삭제 이후, 최대 힙의 무결성을 확인해야 한다. 최대 힙은 모든 상위 node의 value가 하위 node의 value보다 크거나 같아야 하는데
-//요소를 제거하여 이 규칙에 위배 되므로 sift down(선별)이 필요하다.
-//sift down을 수행하려면 현재 value(root, 이전의 마지막 요소)부터 시작하여 왼쪽과 오른쪽 child를 확인한다.
-//두 child 중 하나의 value가 현재 value 보다 큰 경우 부모 node와 swap한다.
+//요소 제거 이후 이 규칙이 위배되면 sift down(선별)이 필요하다.
+//sift down을 수행하려면 현재 value(현재 root, 이전의 마지막 요소)부터 시작하여 왼쪽과 오른쪽 child를 확인한다.
+//두 자식(child) 중 하나의 값(value)이 현재 값(value)보다 큰 경우, 해당 노드를 부모 노드와 교환(swap)한다.
 //양쪽 child의 value가 모두 부모 value보다 크다면, 둘 중 더 큰 value와 부모 node를 swap한다.
 //이후, node의 value가 child의 value보다 크지 않을 때까지 계속 걸러 내려간다. //p.233
 //이렇게 마지막 level에 도달하게 되면, 최대 힙의 속성을 다시 만족하게 된다.
@@ -154,14 +156,17 @@ extension Heap {
         }
     }
 }
-//remove()의 전반적인 시간 복잡도는 O(lon n)이다. Array에서 요소를 바꾸는 데 O(1)이 걸리고, sift down에 O(log n)이 걸린다.
+//remove()의 전체 시간 복잡도는 O(lon n)이다. Array에서 요소를 바꾸는 데 O(1)이 걸리고, sift down에 O(log n)이 걸린다.
 
 
 
 
 //Inserting into a heap
 //heap에서 요소 삽입은 먼저, 삽입하려는 value를 heap의 가장 끝에 추가한다. //p.235
-//remove()에서의 siftDown과는 반대로 삽입한 node와 parent node의 우선 순위를 비교하면서 위로 올라간다.
+//그리고 최대 힙(max heap)의 조건을 확인한다.
+//방금 삽입한 노드(node)가 부모(parent)보다 우선 순위(priority)가 높을 수 있으므로 선별 작업이 필요하다.
+//하지만 이전 remove()에서의 siftDown과는 반대로 삽입한 노드(node)와 부모 노드(parent node)의 우선 순위를 비교하면서 위로 올라간다.
+//방향을 제외하면, siftDown과 유사하다.
 
 //Implementation of insert
 extension Heap {
@@ -185,7 +190,7 @@ extension Heap {
 }
 // • 요소를 Array에 추가한 후, sift up을 해 준다.
 // • siftUp은 현재 node가 상위 node보다 우선 순위가 높을 경우 현재 node를 상위 node와 swap한다.
-//insert()의 전반적인 시간 복잡도는 O(lon n)이다. Array에서 요소를 추가하는 것은 O(1)이 걸리고, sift up에 O(log n)이 걸린다.
+//insert(_:)의 전체 시간 복잡도는 O(lon n)이다. Array에서 요소를 추가하는 것은 O(1)이 걸리고, sift up에 O(log n)이 걸린다.
 
 
 
@@ -212,7 +217,10 @@ extension Heap {
     }
 }
 //p.238
-//heap에서 임의의 요소를 제거하는 것은 O(log n)이다. 하지만 제거하려는 요소의 index를 찾는 방법이 필요하다.
+//siftDown과 siftUp를 모두 호출해야 한다. 5를 제거한다고 가정하면,
+//마지막 요소인 8과 교환(swap)하고 sift up을 연달아 호출해, 힙 조건을 충족하는지 확인해야 한다.
+//7을 제거한다고 가정하면, 7을 마지막 요소인 1과 교환(swap)하고, sift down을 연달아 호출해, 힙 조건을 충족하는지 확인해야 한다.
+//힙(heap)에서 임의의 요소를 제거하는 것은 O(log n)이다. 하지만 제거하려는 요소의 index를 찾을 방법이 아직 없다.
 
 
 
@@ -220,7 +228,8 @@ extension Heap {
 //Searching for an element in a heap
 //삭제하려는 요소의 index를 찾으려면 heap에서 검색을 수행해야 한다. 불행히도 heap은 빠른 검색을 목적으로 고안되지 않았다.
 //이진 탐색 트리(BST)를 사용하면 순회에 O(log n)이 걸리지만,
-//heap은 Array를 사용하고, Array의 요소도 node의 순서와 다르기 때문에 이진 검색을 수행할 수 없다.
+//이진 탐색 트리(BST)를 사용하면 순회에 O(log n)이 걸리지만 heap은 배열(Array)을 사용하고,
+//배열의 요소도 정렬되어 있지 않기 때문에 이진 탐색(binary search)을 수행할 수 없다.
 //최악의 경우 Array의 모든 요소를 확인해야 하므로 O(n)이 된다.
 extension Heap {
     func index(of element: Element, startingAt i: Int) -> Int? {
@@ -273,9 +282,10 @@ extension Heap {
         }
     }
 }
-//추가된 init는 추가적인 매개변수를 사용해 이미 있는 Array로 heap을 구성한다.
-//힙의 특성을 만족시키기 위해, leaf가 아닌 첫 node에서 시작하여 Array를 거꾸로 반복하면서 모든 상위 node를 선별한다.
-//leaf node를 sift down 하는 것은 의미가 없기 때문에, 요소의 절반(상위 node)만 반복한다.
+//추가된 init는 추가적인 매개변수(parameter)를 사용해 이미 있는 배열(Array)로 힙(heap)을 구성한다.
+//힙의 특성을 만족시키기 위해, 잎(leaf)이 아닌 첫 노드(node)에서 시작하여
+//배열(Array)을 거꾸로 반복하면서 모든 상위 노드(node)를 선별(sift down)한다.
+//잎 노드(leaf node)를 sift down 하는 것은 의미가 없기 때문에, 요소의 절반(상위 node)을 반복해 상위 노드(node)만 제거한다.
 
 
 
@@ -293,14 +303,18 @@ while !heap.isEmpty {
     // 1
     // 1
 }
-//정렬 함수로 >를 사용하기 때문에 max heap이 된다.
+//정렬 함수로 >를 사용했기 때문에 최대 힙(max heap)이 되며, remove()를 사용하면 요소(element)가 빌 때까지 하나씩 제거된다.
+//최대 힙(max heap)이므로, 요소가 가장 큰 값(value)부터 하나씩 제거되며 콘솔에 출력된다.
 
 
 
 
 //Key points
-//heap의 각 연산 시간복잡도
-// • remove : O(log n)
-// • insert : O(log n)
-// • search : O(n)
-// • peek : O(1)
+// • 힙(heap) 연산의 시간 복잡도(time complexity)에 대한 요약은 다음과 같다.
+//  - remove : O(log n)
+//  - insert : O(log n)
+//  - search : O(n)
+//  - peek : O(1)
+// • 힙(heap)은 우선 순위(priority)가 가장 높은(highest) 요소 혹은 가장 낮은(lowest) 요소를 유지하는데 적합한 자료구조(data structure)이다.
+// • 힙(heap)에서 항목을 삽입(insert)하거나 제거(remove)할 때마다, 우선 순위(priority) 조건을 충족하는지 확인해야 한다.
+

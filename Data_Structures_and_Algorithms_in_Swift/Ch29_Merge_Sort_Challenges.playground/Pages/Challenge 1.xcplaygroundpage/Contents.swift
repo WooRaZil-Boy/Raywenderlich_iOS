829535144c97b605c719cb8a8de3a993da9b6adc
WooRaZil-Boy <byuncorry@hanmail.net>
2020-03-12 11:58:43 +0900

    Edited Chapter 29: Merge Srot Challenges

--- Data_Structures_and_Algorithms_in_Swift/Ch29_Merge_Sort_Challenges.playground/Pages/Challenge 1.xcplaygroundpage/Contents.swift
+++ Data_Structures_and_Algorithms_in_Swift/Ch29_Merge_Sort_Challenges.playground/Pages/Challenge 1.xcplaygroundpage/Contents.swift
@@ -7,7 +7,7 @@ let size = 1024
 var values: [Int] = []
 // 1
 for i in 0 ..< size {
-  values.append(i)
+    values.append(i)
 }
 //이 코드는 재할당이 반복된다.
 //1에 단일 할당으로 줄일 수 있는 코드를 추가한다.
@@ -21,7 +21,7 @@ for i in 0 ..< size {
 }
 //Array에 추가할 요소의 수를 알고 있다면, reserveCapacity를 사용해 Array의 용량을 미리 설정해 줄 수 있다.
 //Array에 요소를 추가할 때, 해당 Array가 예약된 용량을 초과하면, Array는 더 큰 메모리 영역을 할당하고, 요소를 복사한다.
-//이때 새로운 Storage는 이전 Storage 크기의 2배 이다.
+//이때 새로운 storage는 이전 storage 크기의 2배 이다.
 //하지만, reserveCapacity를 사용해 Array의 용량을 미리 설정하면, 재할당 및 복사연산을 수행할 필요가 없어지므로 속도가 빨라진다.
 //https://zeddios.tistory.com/117
 
